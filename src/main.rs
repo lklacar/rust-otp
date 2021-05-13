2a230de8311f5665fa6395a3294b135158cf312e
Luka Klacar <lukaklacar@gmail.com>
2021-05-13 15:36:41 +0200

    Minor refactor

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ extern crate clap;
 
 use clap::{AppSettings, Clap};
 
-use crate::otp::{otp_decrypt, otp_encrypt};
+use crate::aes::{aes_decrypt, aes_encrypt, CHUNK_SIZE, key_from_string};
+use crate::files::{read_bytes, write_bytes};
+use crate::otp::{otp, rand_array};
 
 mod files;
 mod aes;
@@ -41,6 +43,34 @@ struct Decrypt {
     /// OTP Key
     key: String,
 }
+
+pub fn otp_encrypt(input_file_name: &String) {
+    let file_data = read_bytes(&input_file_name);
+
+    let password = rpassword::prompt_password_stdout("Password: ").unwrap();
+    let aes_key = key_from_string(&password);
+
+    let random_bytes = rand_array((file_data.len() + CHUNK_SIZE - file_data.len() % CHUNK_SIZE) as u32);
+
+    let encrypted_otp_key = aes_encrypt(&random_bytes, &aes_key);
+    let encrypted_data = otp(&file_data, &random_bytes);
+
+    write_bytes(&format_args!("{filename}.enc", filename = input_file_name).to_string(), &encrypted_data);
+    write_bytes(&format_args!("{filename}.key", filename = input_file_name).to_string(), &encrypted_otp_key);
+}
+
+pub fn otp_decrypt(input_file_name: &String, keypath: &String) {
+    let file_data = read_bytes(&input_file_name);
+
+    let password = rpassword::prompt_password_stdout("Password: ").unwrap();
+    let encrypted_otp_key = read_bytes(&keypath);
+    let aes_key = key_from_string(&password);
+    let decrypted_otp_key = aes_decrypt(&encrypted_otp_key, &aes_key);
+
+    let encrypted = otp(&file_data, &decrypted_otp_key);
+    write_bytes(&format_args!("{filename}.dec", filename = input_file_name).to_string(), &encrypted);
+}
+
 
 fn main() {
     let opts: Opts = Opts::parse();
--- src/otp.rs
+++ src/otp.rs
@@ -1,50 +1,20 @@
 use rand::{RngCore, SeedableRng};
 use rand_hc::Hc128Rng;
 
-use crate::aes::{aes_decrypt, aes_encrypt, CHUNK_SIZE, key_from_string};
-use crate::files::{read_bytes, write_bytes};
-
-fn rand_array(length: u32) -> Vec<u8> {
+pub fn rand_array(length: u32) -> Vec<u8> {
     let mut random_generator: Hc128Rng = Hc128Rng::from_entropy();
-    let limit = (length + CHUNK_SIZE as u32 - length % CHUNK_SIZE as u32) / 4;
-    return (0..limit).flat_map(|_| {
+    return (0..length / 4).flat_map(|_| {
         let rand = random_generator.next_u32();
         let rand_bytes = rand.to_be_bytes();
         return rand_bytes.to_vec().into_iter();
     }).collect();
 }
 
-fn otp(input: &Vec<u8>, key: &Vec<u8>) -> Vec<u8> {
+pub fn otp(input: &Vec<u8>, key: &Vec<u8>) -> Vec<u8> {
     return (0..input.len()).map(|i| {
         let input_byte = input[i];
         let key_byte = key[i];
         let otp_byte = input_byte ^ key_byte;
         return otp_byte;
     }).collect();
-}
-
-pub fn otp_encrypt(input_file_name: &String) {
-    let file = read_bytes(&input_file_name);
-
-    let pass = rpassword::prompt_password_stdout("Password: ").unwrap();
-    let key = key_from_string(&pass);
-    let secure_bytes = rand_array(file.len() as u32);
-
-    let encrypted_key = aes_encrypt(&secure_bytes, &key);
-    let encrypted = otp(&file, &secure_bytes);
-
-    write_bytes(&format_args!("{filename}.enc", filename = input_file_name).to_string(), &encrypted);
-    write_bytes(&format_args!("{filename}.key", filename = input_file_name).to_string(), &encrypted_key);
-}
-
-pub fn otp_decrypt(input_file_name: &String, keypath: &String) {
-    let file = read_bytes(&input_file_name);
-
-    let pass = rpassword::prompt_password_stdout("Password: ").unwrap();
-    let secure_bytes = read_bytes(&keypath);
-    let key = key_from_string(&pass);
-    let decrypted_key = aes_decrypt(&secure_bytes, &key);
-
-    let encrypted = otp(&file, &decrypted_key);
-    write_bytes(&format_args!("{filename}.dec", filename = input_file_name).to_string(), &encrypted);
 }
